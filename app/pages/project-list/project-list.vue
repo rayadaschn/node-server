5e74b5b75a0250c1aafe46b1b2c6706396b62a47
rayadaschn <115447518+rayadaschn@users.noreply.github.com>
2025-11-13 22:31:29 +0800

    feat: 更新模版列表

--- app/pages/project-list/project-list.vue
+++ app/pages/project-list/project-list.vue
@@ -1,13 +1,43 @@
 <template>
   <div>
-    <headerContainer>
+    <headerContainer title="项目列表">
       <template #main-content>
-        <div>
-          <h1>项目列表</h1>
-          <el-table :data="tableData">
-            <el-table-column prop="id" label="项目ID"></el-table-column>
-            <el-table-column prop="name" label="项目名称"></el-table-column>
-          </el-table>
+        <div v-loading="loading">
+          <div v-for="item in modelList" :key="item.id">
+            <div class="model-panel">
+              <el-row>
+                <div class="title">{{ item.model.name }}</div>
+              </el-row>
+              <div class="line">
+                <!-- 分割线 -->
+                <el-divider></el-divider>
+              </div>
+            </div>
+            <el-row>
+              <el-card
+                v-for="projItem in item.project"
+                :key="projItem.name"
+                :title="projItem.name"
+              >
+                <template #header>
+                  <div class="title">
+                    <span>{{ projItem.name }}</span>
+                  </div>
+                </template>
+                <div class="content">{{ projItem.desc }}</div>
+                <template #footer>
+                  <el-row>
+                    <el-button
+                      type="primary"
+                      @click="handleDetailClick(projItem)"
+                    >
+                      查看详情
+                    </el-button>
+                  </el-row>
+                </template>
+              </el-card>
+            </el-row>
+          </div>
         </div>
       </template>
     </headerContainer>
@@ -16,6 +46,40 @@
 
 <script setup>
 import headerContainer from '@/widgets/header-container/header-container.vue'
+import curl from '@/common/curl.js'
+import { template } from 'lodash'
+const { ref, onMounted } = require('vue')
+const loading = ref(false)
+const modelList = ref([])
+
+async function getModelList() {
+  loading.value = true
+  try {
+    const res = await curl({
+      url: '/api/model/list',
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        name: '获取项目列表',
+      },
+    })
+    modelList.value = res.data || []
+  } catch (err) {
+    console.error(err)
+  } finally {
+    loading.value = false
+  }
+}
+
+function handleDetailClick(projItem) {
+  console.log('查看详情', projItem)
+}
+
+onMounted(() => {
+  getModelList()
+})
 </script>
 
 <style scoped></style>
